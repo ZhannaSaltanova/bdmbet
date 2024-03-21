075c870a6a3b88bb7b9f5515b3443de4dc66759a
ZhannaSaltanova <nedikovazhanna@gmail.com>
2024-03-21 18:25:30 +0200

    fix

--- script/script.js
+++ script/script.js
@@ -1,27 +1,36 @@
 
-
-
 document.getElementById('toggleButton').addEventListener('click', function() {
     let burgerMenu = document.querySelector('.burgermenu');
     let menu = document.querySelector('.menu');
 
     burgerMenu.classList.toggle('open');
-    
+
+
     if (window.innerWidth >= 1280) {
-        if (menu.style.display === "none") {
+        if (menu.style.display === "none" || menu.style.display === "") {
             menu.style.display = "flex";
+            burgerMenu.classList.remove('open'); 
         } else {
             menu.style.display = "none";
+            burgerMenu.classList.add('open'); 
         }
     } else {
+        if (menu.style.display === "flex") {
+            burgerMenu.classList.remove('open'); 
+        }
         menu.style.display = "none";
     }
 });
-
 
 window.addEventListener('resize', function() {
     let menu = document.querySelector('.menu');
+    let burgerMenu = document.querySelector('.burgermenu');
+
     if (window.innerWidth < 1280) {
         menu.style.display = "none";
+    } else {
+        burgerMenu.classList.remove('open');
+        menu.style.display = "flex"; 
     }
 });
+
